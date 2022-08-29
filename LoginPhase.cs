3f322241dc348f1b5bec055acd481c32aaf86f07
Damian Mares <damian.mares@totalsoft.ro>
2022-08-29 17:12:40 +0300

    push

--- LoginPhase.cs
+++ LoginPhase.cs
@@ -16,5 +16,30 @@ namespace ConcediuAngajati
         {
             InitializeComponent();
         }
+
+        private Button button1;
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(99, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // LoginPhase
+            // 
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.button1);
+            this.Name = "LoginPhase";
+            this.ResumeLayout(false);
+
+        }
     }
 }
--- PaginaMea.Designer.cs
+++ PaginaMea.Designer.cs
@@ -48,15 +48,17 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::ConcediuAngajati.Properties.Resources._5983150_poza_indisponibila_persoana_prea_urata;
-            this.pictureBox1.Location = new System.Drawing.Point(761, 124);
+            this.pictureBox1.Location = new System.Drawing.Point(870, 165);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(108, 75);
+            this.pictureBox1.Size = new System.Drawing.Size(123, 100);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -64,9 +66,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(58, 21);
+            this.button1.Location = new System.Drawing.Point(66, 28);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.Size = new System.Drawing.Size(130, 31);
             this.button1.TabIndex = 9;
             this.button1.Text = "Pagina Principala";
             this.button1.UseVisualStyleBackColor = true;
@@ -74,9 +77,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(193, 21);
+            this.button2.Location = new System.Drawing.Point(221, 28);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.Size = new System.Drawing.Size(128, 31);
             this.button2.TabIndex = 10;
             this.button2.Text = "Cerere Concediu";
             this.button2.UseVisualStyleBackColor = true;
@@ -84,9 +88,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(329, 21);
+            this.button3.Location = new System.Drawing.Point(376, 28);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(112, 23);
+            this.button3.Size = new System.Drawing.Size(128, 31);
             this.button3.TabIndex = 11;
             this.button3.Text = "Concediile mele";
             this.button3.UseVisualStyleBackColor = true;
@@ -94,71 +99,79 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(366, 110);
+            this.textBox3.Location = new System.Drawing.Point(418, 147);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(116, 23);
+            this.textBox3.Size = new System.Drawing.Size(132, 27);
             this.textBox3.TabIndex = 3;
             this.textBox3.Text = "Email";
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(225, 176);
+            this.textBox6.Location = new System.Drawing.Point(257, 235);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(112, 23);
+            this.textBox6.Size = new System.Drawing.Size(127, 27);
             this.textBox6.TabIndex = 6;
             this.textBox6.Text = "Cnp";
             this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(366, 176);
+            this.textBox8.Location = new System.Drawing.Point(418, 235);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(116, 23);
+            this.textBox8.Size = new System.Drawing.Size(132, 27);
             this.textBox8.TabIndex = 8;
             this.textBox8.Text = "Numar Telefon";
             this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(521, 110);
+            this.textBox4.Location = new System.Drawing.Point(595, 147);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(121, 23);
+            this.textBox4.Size = new System.Drawing.Size(138, 27);
             this.textBox4.TabIndex = 4;
             this.textBox4.Text = "Data nastere";
             this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(225, 110);
+            this.textBox2.Location = new System.Drawing.Point(257, 147);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(112, 23);
+            this.textBox2.Size = new System.Drawing.Size(127, 27);
             this.textBox2.TabIndex = 2;
             this.textBox2.Text = "Prenume";
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(91, 176);
+            this.textBox7.Location = new System.Drawing.Point(104, 235);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(107, 23);
+            this.textBox7.Size = new System.Drawing.Size(122, 27);
             this.textBox7.TabIndex = 7;
             this.textBox7.Text = "Serie";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(91, 110);
+            this.textBox1.Location = new System.Drawing.Point(104, 147);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(107, 23);
+            this.textBox1.Size = new System.Drawing.Size(122, 27);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Nume";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(521, 176);
+            this.textBox5.Location = new System.Drawing.Point(595, 235);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(121, 23);
+            this.textBox5.Size = new System.Drawing.Size(138, 27);
             this.textBox5.TabIndex = 5;
             this.textBox5.Text = "Data angajare";
             this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
@@ -166,9 +179,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(91, 80);
+            this.label1.Location = new System.Drawing.Point(104, 107);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 15);
+            this.label1.Size = new System.Drawing.Size(56, 20);
             this.label1.TabIndex = 12;
             this.label1.Text = "Nume :";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -176,73 +189,83 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(225, 80);
+            this.label2.Location = new System.Drawing.Point(257, 107);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 15);
+            this.label2.Size = new System.Drawing.Size(74, 20);
             this.label2.TabIndex = 13;
             this.label2.Text = "Prenume :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(366, 80);
+            this.label3.Location = new System.Drawing.Point(418, 107);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.Size = new System.Drawing.Size(57, 20);
             this.label3.TabIndex = 14;
             this.label3.Text = "Email : ";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(521, 80);
+            this.label4.Location = new System.Drawing.Point(595, 107);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 15);
+            this.label4.Size = new System.Drawing.Size(100, 20);
             this.label4.TabIndex = 15;
             this.label4.Text = "Data nastere :";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(91, 151);
+            this.label5.Location = new System.Drawing.Point(104, 201);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.Size = new System.Drawing.Size(49, 20);
             this.label5.TabIndex = 16;
             this.label5.Text = "Serie :";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(225, 151);
+            this.label6.Location = new System.Drawing.Point(257, 201);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 15);
+            this.label6.Size = new System.Drawing.Size(42, 20);
             this.label6.TabIndex = 17;
             this.label6.Text = "Cnp :";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(366, 151);
+            this.label7.Location = new System.Drawing.Point(418, 201);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(90, 15);
+            this.label7.Size = new System.Drawing.Size(112, 20);
             this.label7.TabIndex = 18;
             this.label7.Text = "Numar telefon :";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(521, 151);
+            this.label8.Location = new System.Drawing.Point(595, 201);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(85, 15);
+            this.label8.Size = new System.Drawing.Size(110, 20);
             this.label8.TabIndex = 19;
             this.label8.Text = "Data angajare :";
             this.label8.Click += new System.EventHandler(this.label8_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(385, 415);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // PaginaMea
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.ClientSize = new System.Drawing.Size(970, 563);
+            this.ClientSize = new System.Drawing.Size(1109, 751);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -263,6 +286,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "PaginaMea";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -293,5 +317,6 @@
         private Label label6;
         private Label label7;
         private Label label8;
+        private Button button4;
     }
 }
