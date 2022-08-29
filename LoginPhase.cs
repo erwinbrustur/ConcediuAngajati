7304e40ca9daa59aa71f4677b8ddd7a892c6006d
keedow <astelea3@gmail.com>
2022-08-29 17:25:39 +0300

    :)

---
+++ LoginPhase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ConcediuAngajati
+{
+    public partial class LoginPhase : Form
+    {
+        public LoginPhase()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Utilizator_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             
+        }
+    }
+}
