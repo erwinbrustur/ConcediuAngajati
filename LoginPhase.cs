c16662e91ae5f13e0a6880d414b15cadf07c1a1a
Erwin Brustur <erwin.brustur@totalsoft.ro>
2022-08-30 11:05:20 +0300

    update local

--- LoginPhase.cs
+++ LoginPhase.cs
@@ -12,34 +12,46 @@ namespace ConcediuAngajati
 {
     public partial class LoginPhase : Form
     {
+        String userId, userPass;
         public LoginPhase()
         {
             InitializeComponent();
         }
 
-        private Button button1;
-
-        private void InitializeComponent()
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(99, 125);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // LoginPhase
-            // 
-            this.ClientSize = new System.Drawing.Size(282, 253);
-            this.Controls.Add(this.button1);
-            this.Name = "LoginPhase";
-            this.ResumeLayout(false);
 
+        }
+
+        private void Logare_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                File.WriteAllText("credentials.txt", userId.ToString());
+            }
+            PaginaMea rg = new PaginaMea();
+            rg.Show();
+            this.Visible = false;
+        }
+
+        private void Username_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Inregistrare_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginPhase_Load(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                string possibleUserId = File.ReadAllText("credentials.txt");
+            }
+            userId = Username.Text;
+            userPass = Parola.Text;
         }
     }
 }
