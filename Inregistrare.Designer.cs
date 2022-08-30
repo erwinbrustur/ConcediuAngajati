926490741a24f460de832258207fb4f66f4e13a9
Sebastian Andrei <sebastian.andrei@totalsoft.ro>
2022-08-30 11:11:00 +0300

    Inregistrare sper

--- Inregistrare.Designer.cs
+++ Inregistrare.Designer.cs
@@ -29,22 +29,151 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.FieldNume = new System.Windows.Forms.TextBox();
+            this.FieldPrenume = new System.Windows.Forms.TextBox();
+            this.FieldNrTel = new System.Windows.Forms.TextBox();
+            this.FieldUser = new System.Windows.Forms.TextBox();
+            this.FieldPass = new System.Windows.Forms.TextBox();
+            this.FieldConfirmPass = new System.Windows.Forms.TextBox();
+            this.FieldEmail = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(331, 138);
+            this.label1.Location = new System.Drawing.Point(82, 65);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(40, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Nume";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(375, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prenume";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Numar de telefon";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(394, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nume de utilizator";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(82, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Parola";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(327, 210);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Confirmare Parola";
+            // 
+            // FieldNume
+            // 
+            this.FieldNume.Location = new System.Drawing.Point(152, 62);
+            this.FieldNume.Name = "FieldNume";
+            this.FieldNume.Size = new System.Drawing.Size(153, 23);
+            this.FieldNume.TabIndex = 7;
+            // 
+            // FieldPrenume
+            // 
+            this.FieldPrenume.Location = new System.Drawing.Point(436, 65);
+            this.FieldPrenume.Name = "FieldPrenume";
+            this.FieldPrenume.Size = new System.Drawing.Size(153, 23);
+            this.FieldPrenume.TabIndex = 8;
+            // 
+            // FieldNrTel
+            // 
+            this.FieldNrTel.Location = new System.Drawing.Point(152, 109);
+            this.FieldNrTel.Name = "FieldNrTel";
+            this.FieldNrTel.Size = new System.Drawing.Size(153, 23);
+            this.FieldNrTel.TabIndex = 9;
+            // 
+            // FieldUser
+            // 
+            this.FieldUser.Location = new System.Drawing.Point(152, 163);
+            this.FieldUser.Name = "FieldUser";
+            this.FieldUser.Size = new System.Drawing.Size(153, 23);
+            this.FieldUser.TabIndex = 10;
+            // 
+            // FieldPass
+            // 
+            this.FieldPass.Location = new System.Drawing.Point(152, 207);
+            this.FieldPass.Name = "FieldPass";
+            this.FieldPass.Size = new System.Drawing.Size(153, 23);
+            this.FieldPass.TabIndex = 11;
+            // 
+            // FieldConfirmPass
+            // 
+            this.FieldConfirmPass.Location = new System.Drawing.Point(436, 207);
+            this.FieldConfirmPass.Name = "FieldConfirmPass";
+            this.FieldConfirmPass.Size = new System.Drawing.Size(153, 23);
+            this.FieldConfirmPass.TabIndex = 12;
+            // 
+            // FieldEmail
+            // 
+            this.FieldEmail.Location = new System.Drawing.Point(436, 106);
+            this.FieldEmail.Name = "FieldEmail";
+            this.FieldEmail.Size = new System.Drawing.Size(153, 23);
+            this.FieldEmail.TabIndex = 13;
             // 
             // Inregistrare
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FieldEmail);
+            this.Controls.Add(this.FieldConfirmPass);
+            this.Controls.Add(this.FieldPass);
+            this.Controls.Add(this.FieldUser);
+            this.Controls.Add(this.FieldNrTel);
+            this.Controls.Add(this.FieldPrenume);
+            this.Controls.Add(this.FieldNume);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Inregistrare";
             this.Text = "Form1";
@@ -56,5 +185,18 @@
         #endregion
 
         private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox FieldNume;
+        private TextBox FieldPrenume;
+        private TextBox FieldNrTel;
+        private TextBox FieldUser;
+        private TextBox FieldPass;
+        private TextBox FieldConfirmPass;
+        private TextBox FieldEmail;
     }
 }
