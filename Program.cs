73a944e3d3c42b9560fa2eac3569d92b5a5914d9
keedow <astelea3@gmail.com>
2022-08-31 13:58:38 +0300

    MergeVerifLogin

--- Program.cs
+++ Program.cs
@@ -11,8 +11,7 @@ namespace ConcediuAngajati
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new LoginPhase()
-                );
+            Application.Run(new PaginaMea());
         }
     }
 }
