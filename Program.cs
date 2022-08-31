0711ce26f29ffe2e78a0e4e3b26c63363e66afe5
Damian Mares <damian.mares@totalsoft.ro>
2022-08-31 10:04:11 +0300

    nnn

--- Program.cs
+++ Program.cs
@@ -11,7 +11,8 @@ namespace ConcediuAngajati
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new PaginaPrincipala.PaginaPrincipala());
+            Application.Run(new LoginPhase()
+                );
         }
     }
 }
