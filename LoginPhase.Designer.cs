62a153ab58cbb53578c6258a7e1fbac975d3041a
keedow <astelea3@gmail.com>
2022-09-01 12:48:44 +0300

    firsttry

--- LoginPhase.Designer.cs
+++ LoginPhase.Designer.cs
@@ -58,6 +58,7 @@
             // 
             // button1
             // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Location = new System.Drawing.Point(527, 374);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
@@ -68,6 +69,7 @@
             // 
             // button2
             // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Location = new System.Drawing.Point(608, 374);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(104, 23);
