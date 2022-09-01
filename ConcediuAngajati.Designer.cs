fbbab629b62ce2f8a78faa18a303436deaffe8a7
Erwin Brustur <erwin.brustur@totalsoft.ro>
2022-09-01 10:31:45 +0300

    dsa

--- ConcediuAngajati.Designer.cs
+++ ConcediuAngajati.Designer.cs
@@ -28,100 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dataGridViewCheckBoxColumn3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Inlocuitor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.checkAccept = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dataGridViewCheckBoxColumn3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Respins = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.checkAccept)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // checkAccept
             // 
-            this.dataGridView1.AllowUserToOrderColumns = true;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.checkAccept.AllowUserToOrderColumns = true;
+            this.checkAccept.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.checkAccept.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dataGridViewCheckBoxColumn3,
-            this.dataGridViewTextBoxColumn3,
-            this.dataGridViewTextBoxColumn4,
-            this.dataGridViewTextBoxColumn5,
-            this.Inlocuitor});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 65);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(678, 373);
-            this.dataGridView1.TabIndex = 13;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // dataGridViewCheckBoxColumn3
-            // 
-            this.dataGridViewCheckBoxColumn3.HeaderText = "Check";
-            this.dataGridViewCheckBoxColumn3.MinimumWidth = 6;
-            this.dataGridViewCheckBoxColumn3.Name = "dataGridViewCheckBoxColumn3";
-            this.dataGridViewCheckBoxColumn3.Width = 125;
-            // 
-            // dataGridViewTextBoxColumn3
-            // 
-            this.dataGridViewTextBoxColumn3.HeaderText = "Nume";
-            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
-            this.dataGridViewTextBoxColumn3.Width = 125;
-            // 
-            // dataGridViewTextBoxColumn4
-            // 
-            this.dataGridViewTextBoxColumn4.HeaderText = "Data Inceput";
-            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
-            this.dataGridViewTextBoxColumn4.Width = 125;
-            // 
-            // dataGridViewTextBoxColumn5
-            // 
-            this.dataGridViewTextBoxColumn5.HeaderText = "Data Sfarsit";
-            this.dataGridViewTextBoxColumn5.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
-            this.dataGridViewTextBoxColumn5.Width = 125;
-            // 
-            // Inlocuitor
-            // 
-            this.Inlocuitor.HeaderText = "Inlocuitor";
-            this.Inlocuitor.MinimumWidth = 6;
-            this.Inlocuitor.Name = "Inlocuitor";
-            this.Inlocuitor.Width = 125;
+            this.Respins});
+            this.checkAccept.Location = new System.Drawing.Point(10, 49);
+            this.checkAccept.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkAccept.Name = "checkAccept";
+            this.checkAccept.RowHeadersWidth = 51;
+            this.checkAccept.RowTemplate.Height = 29;
+            this.checkAccept.Size = new System.Drawing.Size(769, 278);
+            this.checkAccept.TabIndex = 13;
+            this.checkAccept.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(259, 9);
+            this.label1.Location = new System.Drawing.Point(227, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(250, 50);
+            this.label1.Size = new System.Drawing.Size(219, 38);
             this.label1.TabIndex = 12;
             this.label1.Text = "Concediu Angajati";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // dataGridViewCheckBoxColumn3
+            // 
+            this.dataGridViewCheckBoxColumn3.HeaderText = "Accept";
+            this.dataGridViewCheckBoxColumn3.Name = "dataGridViewCheckBoxColumn3";
+            this.dataGridViewCheckBoxColumn3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Respins
+            // 
+            this.Respins.HeaderText = "Respins";
+            this.Respins.Name = "Respins";
+            this.Respins.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Respins.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
             // ConcediuAngajati
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(791, 338);
+            this.Controls.Add(this.checkAccept);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ConcediuAngajati";
             this.Text = "ConcediuAngajati";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkAccept)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private DataGridView dataGridView1;
-        private DataGridViewCheckBoxColumn dataGridViewCheckBoxColumn3;
-        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
-        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
-        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
-        private DataGridViewTextBoxColumn Inlocuitor;
+        private DataGridView checkAccept;
         private Label label1;
+        private DataGridViewCheckBoxColumn dataGridViewCheckBoxColumn3;
+        private DataGridViewCheckBoxColumn Respins;
     }
 }
--- ConcediuAngajati.cs
+++ ConcediuAngajati.cs
@@ -18,33 +18,39 @@ namespace ConcediuAngajati
         {
             InitializeComponent();
             connectionString = @"Data Source=ts2112\SQLEXPRESS;Initial Catalog=StrangerThings;User ID=internship2022;Password=int";
+            extragereConcediiDB();
         }
 
-        public List<Concediu> extragereTipConcediiDB()
+        public void extragereConcediiDB()
         {
             List<Concediu> listaConcedii = new List<Concediu>();
-            string selectSQL = "SELECT * from Concediu";
+            string selectSQL = "SELECT a.nume + ' ' + a.prenume as Nume, Convert(date, c.dataInceput) as 'Data Inceput', Convert(date, c.dataSfarsit) as 'Data Sfarsit', a2.nume + ' ' + a2.prenume as Inlocuitor, c.comentarii as 'Comentarii' FROM Angajat a JOIN Concediu c ON a.id = c.angajatId JOIN Angajat a2 ON a2.id = c.inlocuitorId";
             SqlConnection conexiune = new SqlConnection(connectionString);
             SqlCommand querySelect = new SqlCommand(selectSQL);
             try
             {
                 conexiune.Open();
                 querySelect.Connection = conexiune;
-                SqlDataReader reader = querySelect.ExecuteReader();
+                //SqlDataReader reader = querySelect.ExecuteReader();
 
-                while (reader.Read())
-                {
-                    Concediu c = new Concediu(Convert.ToInt32(reader[0].ToString()), Convert.ToInt32(reader[1].ToString()), Convert.ToDateTime(reader[2].ToString()), Convert.ToDateTime(reader[3].ToString()), Convert.ToInt32(reader[4].ToString()), reader[5].ToString(), Convert.ToInt32(reader[6].ToString()), Convert.ToInt32(reader[7].ToString()));
-                    listaConcedii.Add(c);
-                }
+                //while (reader.Read())
+                //{
+                //    Concediu c = new Concediu(Convert.ToInt32(reader[0].ToString()), Convert.ToInt32(reader[1].ToString()), Convert.ToDateTime(reader[2].ToString()), Convert.ToDateTime(reader[3].ToString()), Convert.ToInt32(reader[4].ToString()), reader[5].ToString(), Convert.ToInt32(reader[6].ToString()), Convert.ToInt32(reader[7].ToString()));
+                //    listaConcedii.Add(c);
+                //}
+                
+                DataTable dt = new DataTable();
+                SqlDataAdapter adapt = new SqlDataAdapter(selectSQL, conexiune);
+                adapt.Fill(dt);
+                checkAccept.DataSource = dt;
+          
 
-
-                return listaConcedii;
+                
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-                return null;
+                
             }
             finally
             {
@@ -76,6 +82,11 @@ namespace ConcediuAngajati
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void checkRespins_CheckedChanged(object sender, EventArgs e)
         {
 
         }
