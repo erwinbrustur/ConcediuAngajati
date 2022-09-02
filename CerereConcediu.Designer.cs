ea2295d39844e96d174c85065742e6fe885be266
keedow <astelea3@gmail.com>
2022-09-02 12:11:05 +0300

    .

--- CerereConcediu.Designer.cs
+++ CerereConcediu.Designer.cs
@@ -57,11 +57,11 @@
             // 
             this.PaginaMea.BackColor = System.Drawing.Color.Transparent;
             this.PaginaMea.FlatAppearance.BorderSize = 0;
-            this.PaginaMea.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.PaginaMea.ForeColor = System.Drawing.Color.White;
+            this.PaginaMea.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PaginaMea.ForeColor = System.Drawing.Color.Maroon;
             this.PaginaMea.Location = new System.Drawing.Point(33, 15);
             this.PaginaMea.Name = "PaginaMea";
-            this.PaginaMea.Size = new System.Drawing.Size(88, 23);
+            this.PaginaMea.Size = new System.Drawing.Size(101, 31);
             this.PaginaMea.TabIndex = 0;
             this.PaginaMea.Text = "Pagina mea";
             this.PaginaMea.UseVisualStyleBackColor = false;
@@ -71,11 +71,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
             this.label1.Location = new System.Drawing.Point(34, 60);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 15);
+            this.label1.Size = new System.Drawing.Size(101, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Tip concediu :";
             // 
@@ -86,9 +86,10 @@
             this.dateTimePicker1.CalendarTitleBackColor = System.Drawing.Color.DarkSalmon;
             this.dateTimePicker1.CalendarTitleForeColor = System.Drawing.Color.Maroon;
             this.dateTimePicker1.CalendarTrailingForeColor = System.Drawing.Color.DarkSalmon;
-            this.dateTimePicker1.Location = new System.Drawing.Point(179, 85);
+            this.dateTimePicker1.Location = new System.Drawing.Point(205, 113);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.Size = new System.Drawing.Size(228, 27);
             this.dateTimePicker1.TabIndex = 3;
             this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
@@ -96,18 +97,18 @@
             // 
             this.dateTimePicker2.Location = new System.Drawing.Point(415, 41);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 27);
             this.dateTimePicker2.TabIndex = 4;
             // 
             // Trimite
             // 
             this.Trimite.BackColor = System.Drawing.Color.Transparent;
             this.Trimite.FlatAppearance.BorderSize = 0;
-            this.Trimite.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.Trimite.ForeColor = System.Drawing.Color.White;
+            this.Trimite.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Trimite.ForeColor = System.Drawing.Color.Maroon;
             this.Trimite.Location = new System.Drawing.Point(591, 245);
             this.Trimite.Name = "Trimite";
-            this.Trimite.Size = new System.Drawing.Size(75, 23);
+            this.Trimite.Size = new System.Drawing.Size(86, 31);
             this.Trimite.TabIndex = 5;
             this.Trimite.Text = "Trimite";
             this.Trimite.UseVisualStyleBackColor = false;
@@ -122,25 +123,25 @@
             this.medicalToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.ShowCheckMargin = true;
-            this.contextMenuStrip1.Size = new System.Drawing.Size(139, 70);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(154, 76);
             this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
             // 
             // odihnaToolStripMenuItem
             // 
             this.odihnaToolStripMenuItem.Name = "odihnaToolStripMenuItem";
-            this.odihnaToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.odihnaToolStripMenuItem.Size = new System.Drawing.Size(153, 24);
             this.odihnaToolStripMenuItem.Text = "Odihna";
             // 
             // remoteToolStripMenuItem
             // 
             this.remoteToolStripMenuItem.Name = "remoteToolStripMenuItem";
-            this.remoteToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.remoteToolStripMenuItem.Size = new System.Drawing.Size(153, 24);
             this.remoteToolStripMenuItem.Text = "Remote";
             // 
             // medicalToolStripMenuItem
             // 
             this.medicalToolStripMenuItem.Name = "medicalToolStripMenuItem";
-            this.medicalToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.medicalToolStripMenuItem.Size = new System.Drawing.Size(153, 24);
             this.medicalToolStripMenuItem.Text = "Medical";
             // 
             // cbTipConcediu
@@ -150,9 +151,10 @@
             this.cbTipConcediu.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.cbTipConcediu.ForeColor = System.Drawing.Color.White;
             this.cbTipConcediu.FormattingEnabled = true;
-            this.cbTipConcediu.Location = new System.Drawing.Point(34, 85);
+            this.cbTipConcediu.Location = new System.Drawing.Point(39, 113);
+            this.cbTipConcediu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cbTipConcediu.Name = "cbTipConcediu";
-            this.cbTipConcediu.Size = new System.Drawing.Size(121, 23);
+            this.cbTipConcediu.Size = new System.Drawing.Size(138, 28);
             this.cbTipConcediu.TabIndex = 7;
             this.cbTipConcediu.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -161,22 +163,22 @@
             this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(34)))), ((int)(((byte)(58)))));
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.Color.Maroon;
             this.textBox1.Location = new System.Drawing.Point(639, 85);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(115, 23);
+            this.textBox1.Size = new System.Drawing.Size(131, 31);
             this.textBox1.TabIndex = 8;
             this.textBox1.Text = "0";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
             this.label2.Location = new System.Drawing.Point(639, 60);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(125, 15);
+            this.label2.Size = new System.Drawing.Size(156, 20);
             this.label2.TabIndex = 9;
             this.label2.Text = " Numar zile concediu :";
             // 
@@ -185,31 +187,28 @@
             this.cbInlocuitor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(34)))), ((int)(((byte)(58)))));
             this.cbInlocuitor.ForeColor = System.Drawing.Color.White;
             this.cbInlocuitor.FormattingEnabled = true;
-            this.cbInlocuitor.Location = new System.Drawing.Point(411, 159);
+            this.cbInlocuitor.Location = new System.Drawing.Point(470, 212);
+            this.cbInlocuitor.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cbInlocuitor.Name = "cbInlocuitor";
-            this.cbInlocuitor.Size = new System.Drawing.Size(121, 23);
+            this.cbInlocuitor.Size = new System.Drawing.Size(138, 28);
             this.cbInlocuitor.TabIndex = 10;
             this.cbInlocuitor.SelectedIndexChanged += new System.EventHandler(this.cbInlocuitor_SelectedIndexChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(411, 132);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 15);
+            this.label3.Size = new System.Drawing.Size(79, 20);
             this.label3.TabIndex = 11;
             this.label3.Text = "Inlocuitor :";
             // 
             // rtbComentarii
             // 
-            this.rtbComentarii.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(34)))), ((int)(((byte)(58)))));
-            this.rtbComentarii.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.rtbComentarii.ForeColor = System.Drawing.Color.White;
+            this.rtbComentarii.BackColor = System.Drawing.Color.DarkSalmon;
             this.rtbComentarii.Location = new System.Drawing.Point(34, 159);
             this.rtbComentarii.Name = "rtbComentarii";
-            this.rtbComentarii.Size = new System.Drawing.Size(345, 96);
+            this.rtbComentarii.Size = new System.Drawing.Size(394, 127);
             this.rtbComentarii.TabIndex = 12;
             this.rtbComentarii.Text = "";
             this.rtbComentarii.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
@@ -217,42 +216,35 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(34, 132);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(72, 15);
+            this.label4.Size = new System.Drawing.Size(89, 20);
             this.label4.TabIndex = 13;
             this.label4.Text = "Comentarii :";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.ForeColor = System.Drawing.Color.White;
             this.label5.Location = new System.Drawing.Point(178, 58);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(39, 15);
+            this.label5.Size = new System.Drawing.Size(51, 20);
             this.label5.TabIndex = 14;
             this.label5.Text = "De la :";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.ForeColor = System.Drawing.Color.White;
             this.label6.Location = new System.Drawing.Point(411, 58);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(51, 15);
+            this.label6.Size = new System.Drawing.Size(63, 20);
             this.label6.TabIndex = 15;
             this.label6.Text = "Pana la :";
             // 
             // dateTimePicker3
             // 
-            this.dateTimePicker3.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(34)))), ((int)(((byte)(58)))));
             this.dateTimePicker3.Location = new System.Drawing.Point(411, 85);
             this.dateTimePicker3.Name = "dateTimePicker3";
-            this.dateTimePicker3.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker3.Size = new System.Drawing.Size(228, 27);
             this.dateTimePicker3.TabIndex = 16;
             this.dateTimePicker3.ValueChanged += new System.EventHandler(this.dateTimePicker3_ValueChanged);
             // 
@@ -261,11 +253,11 @@
             this.btnInchidereCC.BackColor = System.Drawing.Color.Transparent;
             this.btnInchidereCC.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnInchidereCC.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnInchidereCC.ForeColor = System.Drawing.Color.White;
+            this.btnInchidereCC.ForeColor = System.Drawing.Color.Maroon;
             this.btnInchidereCC.Location = new System.Drawing.Point(757, 6);
             this.btnInchidereCC.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnInchidereCC.Name = "btnInchidereCC";
-            this.btnInchidereCC.Size = new System.Drawing.Size(37, 43);
+            this.btnInchidereCC.Size = new System.Drawing.Size(42, 57);
             this.btnInchidereCC.TabIndex = 17;
             this.btnInchidereCC.Text = "X";
             this.btnInchidereCC.UseVisualStyleBackColor = false;
@@ -273,10 +265,8 @@
             // 
             // CerereConcediu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnInchidereCC);
             this.Controls.Add(this.dateTimePicker3);
@@ -295,6 +285,7 @@
             this.Controls.Add(this.PaginaMea);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "CerereConcediu";
             this.Text = "CerereConcediu";
             this.Load += new System.EventHandler(this.CerereConcediu_Load);
--- ConcediuAngajati.Designer.cs
+++ ConcediuAngajati.Designer.cs
@@ -37,17 +37,15 @@
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnInchidereCA = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvConcedii)).BeginInit();
             this.SuspendLayout();
             // 
             // Actualizare
             // 
-            this.Actualizare.BackColor = System.Drawing.Color.Transparent;
-            this.Actualizare.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Actualizare.ForeColor = System.Drawing.Color.White;
             this.Actualizare.Location = new System.Drawing.Point(624, 26);
             this.Actualizare.Name = "Actualizare";
-            this.Actualizare.Size = new System.Drawing.Size(107, 23);
+            this.Actualizare.Size = new System.Drawing.Size(122, 31);
             this.Actualizare.TabIndex = 0;
             this.Actualizare.Text = "Actualizare";
             this.Actualizare.UseVisualStyleBackColor = false;
@@ -56,9 +54,10 @@
             // cbStareConcediu
             // 
             this.cbStareConcediu.FormattingEnabled = true;
-            this.cbStareConcediu.Location = new System.Drawing.Point(31, 21);
+            this.cbStareConcediu.Location = new System.Drawing.Point(35, 28);
+            this.cbStareConcediu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cbStareConcediu.Name = "cbStareConcediu";
-            this.cbStareConcediu.Size = new System.Drawing.Size(121, 23);
+            this.cbStareConcediu.Size = new System.Drawing.Size(138, 28);
             this.cbStareConcediu.TabIndex = 1;
             this.cbStareConcediu.SelectedIndexChanged += new System.EventHandler(this.cbStareConcediu_SelectedIndexChanged);
             // 
@@ -72,11 +71,13 @@
             this.Column3,
             this.Column4,
             this.Column5});
-            this.dgvConcedii.Location = new System.Drawing.Point(31, 73);
+            this.dgvConcedii.Location = new System.Drawing.Point(35, 97);
+            this.dgvConcedii.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dgvConcedii.MultiSelect = false;
             this.dgvConcedii.Name = "dgvConcedii";
+            this.dgvConcedii.RowHeadersWidth = 51;
             this.dgvConcedii.RowTemplate.Height = 25;
-            this.dgvConcedii.Size = new System.Drawing.Size(722, 270);
+            this.dgvConcedii.Size = new System.Drawing.Size(825, 360);
             this.dgvConcedii.TabIndex = 2;
             this.dgvConcedii.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvConcedii_CellContentClick);
             this.dgvConcedii.RowStateChanged += new System.Windows.Forms.DataGridViewRowStateChangedEventHandler(this.dgvConcedii_RowStateChanged);
@@ -85,39 +86,59 @@
             // Column1
             // 
             this.Column1.HeaderText = "Nume";
+            this.Column1.MinimumWidth = 6;
             this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
             // 
             // Column2
             // 
             this.Column2.HeaderText = "Data Inceput";
+            this.Column2.MinimumWidth = 6;
             this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
             // 
             // Column3
             // 
             this.Column3.HeaderText = "Data Sfarsit";
+            this.Column3.MinimumWidth = 6;
             this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
             // 
             // Column4
             // 
             this.Column4.HeaderText = "Inlocuitor";
+            this.Column4.MinimumWidth = 6;
             this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
             // 
             // Column5
             // 
             this.Column5.HeaderText = "Comentarii";
+            this.Column5.MinimumWidth = 6;
             this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // btnInchidereCA
+            // 
+            this.btnInchidereCA.BackColor = System.Drawing.Color.Transparent;
+            this.btnInchidereCA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInchidereCA.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnInchidereCA.Location = new System.Drawing.Point(865, 12);
+            this.btnInchidereCA.Name = "btnInchidereCA";
+            this.btnInchidereCA.Size = new System.Drawing.Size(37, 43);
+            this.btnInchidereCA.TabIndex = 3;
+            this.btnInchidereCA.Text = "X";
+            this.btnInchidereCA.UseVisualStyleBackColor = false;
+            this.btnInchidereCA.Click += new System.EventHandler(this.btnInchidereCA_Click);
             // 
             // ConcediuAngajati
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.dgvConcedii);
             this.Controls.Add(this.cbStareConcediu);
             this.Controls.Add(this.Actualizare);
-            this.DoubleBuffered = true;
             this.Name = "ConcediuAngajati";
             this.Text = "ConcediuAngajati";
             ((System.ComponentModel.ISupportInitialize)(this.dgvConcedii)).EndInit();
@@ -135,5 +156,6 @@
         private DataGridViewTextBoxColumn Column3;
         private DataGridViewTextBoxColumn Column4;
         private DataGridViewTextBoxColumn Column5;
+        private Button btnInchidereCA;
     }
 }
