4e34b43d8a3f23f554801b509853a2b370370e01
Erwin Brustur <erwin.brustur@totalsoft.ro>
2022-09-06 16:03:13 +0300

    dsa

--- LoginPhase.cs
+++ LoginPhase.cs
@@ -11,15 +11,19 @@ using System.Windows.Forms;
 using System.Security.Cryptography;
 using System.Reflection.Metadata;
 using ProiectASP.Models;
+using Newtonsoft.Json;
 
 namespace ConcediuAngajati
 {
     public partial class LoginPhase : Form
     {
+        static readonly HttpClient client = new HttpClient();
         public LoginPhase()
         {
             InitializeComponent();
-            
+           
+
+
         }
 
 
@@ -37,96 +41,44 @@ namespace ConcediuAngajati
             return Convert.ToHexString(byteArray);
         }
 
-        public Angajat ExtractAngajatFromSql(string user)
+
+        public async void extragereAngajatByUsername()
         {
-            SqlConnection conexiune = new SqlConnection(@"Data Source=ts2112\SQLEXPRESS;Initial Catalog=StrangerThings;User ID=internship2022;Password=int");
-            conexiune.Open();
-            string CommandLine = "SELECT * FROM Angajat WHERE email='" + user + "@totalsoft.ro'";
-            SqlCommand cmd = new SqlCommand(CommandLine, conexiune);
-            SqlDataReader dr = cmd.ExecuteReader();
-            Angajat angajat = null;
-            if (dr.Read())
-            {
-                // Boolean d = Convert.ToBoolean(dr[12].ToString());
-                // MessageBox.Show(dr[5].ToString());
-
-                //  MessageBox.Show(Convert.ToInt32(dr[0].ToString()) + dr[1].ToString() + dr[2].ToString()+ dr[3].ToString()+ dr[4].ToString()+ Convert.ToDateTime(dr[5].ToString())+ Convert.ToDateTime(dr[6].ToString())+ dr[7].ToString()+ dr[8].ToString()+ dr[9].ToString(), dr[10].ToString()+ Convert.ToBoolean(dr[12].ToString())+ Convert.ToInt32(dr[13].ToString()));
-                int id = Convert.ToInt32(dr[0].ToString());
-                string nume = dr[1].ToString();
-                string prenume = dr[2].ToString();
-                string email = dr[3].ToString();
-                string parola = dr[4].ToString();
-                DateTime dataAngajare = Convert.ToDateTime(dr[5].ToString());
-                DateTime dataNastere = Convert.ToDateTime(dr[6].ToString());
-                string cnp = dr[7].ToString();
-                string serie = dr[8].ToString();
-                string numar = dr[9].ToString();
-                string telefon = dr[10].ToString();
-                byte[] poza = (byte[])dr[11];
-                bool esteAdmin = Convert.ToBoolean(dr[12].ToString());
-                int managerId = Convert.ToInt32(dr[13].ToString());
-                angajat = new Angajat() { Id = id, Nume = nume, Prenume = prenume, Email = email, Parola = parola, DataAngajare = dataAngajare, DataNasterii = dataNastere, Cnp = cnp, Serie = serie, No = numar, NrTelefon = telefon, Poza = poza, EsteAdmin = esteAdmin, ManagerId = managerId };
-            }
-            conexiune.Close();
-            return angajat;
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-            String user = textBox1.Text;
-
-            SqlConnection sqlConnection = new SqlConnection(@"Data Source=ts2112\SQLEXPRESS;Initial Catalog=StrangerThings;User ID=internship2022;Password=int");
-
-            sqlConnection.Open();
-
-            // user din textbox verifica cu user din sql
-            SqlCommand UserCheck = new SqlCommand(@"SELECT parola FROM Angajat WHERE email = '" + user +"@totalsoft.ro'",sqlConnection);
-            //String parolaDinbaza = (String)UserCheck.ExecuteScalar();
-            //MessageBox.Show(parolaDinbaza);
-            
-            SqlDataReader reader = UserCheck.ExecuteReader();
-            
-            String parolaDinbaza = "";
-
-            if (reader.Read())
+            try
             {
 
-                parolaDinbaza = reader[0].ToString();
+                HttpResponseMessage response = await client.GetAsync("http://localhost:5096/GetAngajatByUsername?username=" + textBox1.Text + "@totalsoft.ro&parola=" + Hash(textBox2.Text));
+                response.EnsureSuccessStatusCode();
+                string responsivebody = await response.Content.ReadAsStringAsync();
 
-                if (parolaDinbaza == Hash(textBox2.Text))
+                Angajat angajat = JsonConvert.DeserializeObject<Angajat>(responsivebody);
+
+                if (angajat != null)
                 {
-                    string nume, prenume, email, parola, nrTelefon, cnp, serie, no;
-                    DateTime dataAngajare, dataNasterii;
-                    byte[] poza;
-                    bool esteAdmin;
-                    int managerId,id;
-
-
-
-                    Angajat angajat = ExtractAngajatFromSql(user);
-
                     PaginaPrincipala.PaginaPrincipala ppg = new PaginaPrincipala.PaginaPrincipala(angajat);
-                   // MessageBox.Show("User: " + angajat.Id + angajat.Nume + angajat.Prenume + angajat.Email + angajat.ManagerId);
                     ppg.Show();
                     this.Hide();
                 }
                 else
                 {
-                    MessageBox.Show("Parola incorecta!");
+                    MessageBox.Show("Parola/Username incorect");
                 }
+               
+
 
             }
-            else
+            catch(Exception e)
             {
-                MessageBox.Show("Utilizator gresit!");
+                MessageBox.Show(e.Message);
+                
             }
-                //MessageBox.Show(parolaDinbaza);
-            
-          
 
-            sqlConnection.Close();
-            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            extragereAngajatByUsername();
             
             
         }
